3d145e859d40e0a30ef2f886d4dfcf408c9928f4
clarkzjw <i@jinwei.me>
2022-03-16 12:50:45 -0700

    + calculate bandwidth

--- monitor.py
+++ monitor.py
@@ -16,9 +16,13 @@
 # Modified from ryu/app/simple_monitor_13.py
 # https://github.com/faucetsdn/ryu/blob/master/ryu/app/simple_monitor_13.py
 
+from collections import defaultdict
+from dataclasses import dataclass
 from datetime import datetime
 from operator import attrgetter
+from pprint import pprint
 
+import matplotlib.pyplot as plt
 import networkx as nx
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
@@ -27,24 +31,30 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.lib import hub
-from ryu.ofproto import ofproto_v1_3
+from ryu.ofproto import ofproto_v1_4
 from ryu.topology import event
-from ryu.topology.api import get_switch, get_link
-import copy
-import matplotlib.pyplot as plt
-
-from pprint import pprint
 
 # Monitor interval in seconds
-Interval = 10
+Interval = 1
 MongoURL = "127.0.0.1"
 # ConnectedSW = {"0000c699ecb9ea46": "0000aae305428d4a"}
 # Switches = ["0000c699ecb9ea46", "0000aae305428d4a"]
 SameLink = {"02:92:b4:89:d6:8f": "02:2a:41:1e:70:d0"}
 
 
+@dataclass
+class Stat:
+    prev_rx_bytes_count: int = 0
+    prev_tx_bytes_count: int = 0
+    prev_duration_sec: int = 0
+    prev_duration_nsec: int = 0
+
+
+PrevStats = defaultdict(lambda: defaultdict(Stat))
+
+
 class SimpleMonitor13(simple_switch_13.SimpleSwitch13):
-    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    OFP_VERSIONS = [ofproto_v1_4.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(SimpleMonitor13, self).__init__(*args, **kwargs)
@@ -97,25 +107,61 @@ class SimpleMonitor13(simple_switch_13.SimpleSwitch13):
     @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
     def _port_stats_reply_handler(self, ev):
         body = ev.msg.body
-        self.logger.info('datapath         port     '
+        dpid = "%016x" % ev.msg.datapath.id
+        cur_rx_throughput = 0
+        cur_tx_throughput = 0
+        if dpid == "0000c699ecb9ea46":
+            return
+        self.logger.info('\n\ndatapath         port     '
                          'rx-pkts  rx-bytes rx-error '
-                         'tx-pkts  tx-bytes tx-error')
+                         'tx-pkts  tx-bytes tx-error '
+                         'rx-bandwidth tx-bandwidth')
         self.logger.info('---------------- -------- '
                          '-------- -------- -------- '
-                         '-------- -------- --------')
+                         '-------- -------- --------'
+                         '------------ ------------')
         for stat in sorted(body, key=attrgetter('port_no')):
-            self.logger.info('%016x %8x %8d %8d %8d %8d %8d %8d',
-                             ev.msg.datapath.id, stat.port_no,
-                             stat.rx_packets, stat.rx_bytes, stat.rx_errors,
-                             stat.tx_packets, stat.tx_bytes, stat.tx_errors)
+            # self.logger.info('%016x %8x %8d %8d %8d %8d %8d %8d',
+            #                  ev.msg.datapath.id, stat.port_no,
+            #                  stat.rx_packets, stat.rx_bytes, stat.rx_errors,
+            #                  stat.tx_packets, stat.tx_bytes, stat.tx_errors)
             if stat.port_no != 0xfffffffe:
+                # pprint(vars(stat))
+                prev_stat = PrevStats[dpid][stat.port_no]
+
+                delta_rx_bytes_count = stat.rx_bytes - prev_stat.prev_rx_bytes_count
+                delta_tx_bytes_count = stat.tx_bytes - prev_stat.prev_tx_bytes_count
+                delta_duration_sec = stat.duration_sec - prev_stat.prev_duration_sec
+                delta_duration_nsec = stat.duration_nsec - prev_stat.prev_duration_nsec
+
+                if delta_duration_sec + (delta_duration_nsec / 1000000000.0) == 0:
+                    cur_rx_throughput = 0
+                    cur_tx_throughput = 0
+                else:
+                    # Mbit/sec
+                    cur_rx_throughput = delta_rx_bytes_count / (
+                                delta_duration_sec + (delta_duration_nsec / 1000000000.0)) * 8.0 / 1000000
+                    cur_tx_throughput = delta_tx_bytes_count / (
+                                delta_duration_sec + (delta_duration_nsec / 1000000000.0)) * 8.0 / 1000000
+
                 post = {"date": datetime.utcnow(),
                         "dpid": "%016x" % ev.msg.datapath.id, "portno": stat.port_no,
                         "RXpackets": "%8d" % stat.rx_packets, "RXbytes": "%8d" % stat.rx_bytes,
-                        "RXerrors": "%8d" % stat.rx_errors,
+                        "RXerrors": "%8d" % stat.rx_errors, "RXbandwidth": cur_rx_throughput,
                         "TXpackets": "%8d" % stat.tx_packets, "TXbytes": "%8d" % stat.tx_bytes,
-                        "TXerrors": "%8d" % stat.tx_errors}
+                        "TXerrors": "%8d" % stat.tx_errors, "TXbandwidth": cur_tx_throughput}
                 self.table_port_monitor.insert_one(post)
+                self.logger.info('%016x %8x %8d %8d %8d %8d %8d %8d %f %f',
+                                 ev.msg.datapath.id, stat.port_no,
+                                 stat.rx_packets, stat.rx_bytes, stat.rx_errors,
+                                 stat.tx_packets, stat.tx_bytes, stat.tx_errors,
+                                 cur_rx_throughput, cur_tx_throughput)
+                PrevStats[dpid][stat.port_no] = Stat(
+                    stat.rx_bytes,
+                    stat.tx_bytes,
+                    stat.duration_sec,
+                    stat.duration_nsec
+                )
 
     @set_ev_cls(event.EventPortAdd)
     def _port_add(self, ev):
