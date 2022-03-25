124056a9855c7faf55ebe0f59dd7ddc0f6f9dc27
clarkzjw <i@jinwei.me>
2022-03-25 11:18:08 -0700

    + add _packet_in_handler

--- server/controller.py
+++ server/controller.py
@@ -39,6 +39,15 @@ from ryu.lib import dpid as dpid_lib
 from ryu.lib import hub
 from ryu.ofproto import ofproto_v1_4
 from ryu.topology import event
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_0
+from ryu.lib.mac import haddr_to_bin
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
 from tabulate import tabulate
 from config import MaxInt
 from path import best_target_selection
@@ -129,6 +138,63 @@ class SABRMonitor(simple_switch_13.SimpleSwitch13):
 
         req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
         datapath.send_msg(req)
+
+    # def add_flow(self, datapath, in_port, dst, src, actions):
+    #     ofproto = datapath.ofproto
+    #
+    #     match = datapath.ofproto_parser.OFPMatch(
+    #         in_port=in_port,
+    #         dl_dst=haddr_to_bin(dst), dl_src=haddr_to_bin(src))
+    #
+    #     mod = datapath.ofproto_parser.OFPFlowMod(
+    #         datapath=datapath, match=match, cookie=0,
+    #         command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
+    #         priority=ofproto.OFP_DEFAULT_PRIORITY,
+    #         flags=ofproto.OFPFF_SEND_FLOW_REM, actions=actions)
+    #     datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+        dst = eth.dst
+        src = eth.src
+
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, msg.in_port)
+
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = msg.in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            self.add_flow(datapath, msg.in_port, dst, src, actions)
+
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = datapath.ofproto_parser.OFPPacketOut(
+            datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
+            actions=actions, data=data)
+        datapath.send_msg(out)
 
     # It seems we might only need port stats to calculate throughput and bandwidth
     # So _flow_stats_reply_handler is removed
